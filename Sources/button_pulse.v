6f37d551f3be328a02d254086c2524ec852ab9d0
Hayden Cao <130268332+pileofhay@users.noreply.github.com>
2024-05-19 10:10:48 -0700

    Update button_pulse.v

--- Sources/button_pulse.v
+++ Sources/button_pulse.v
@@ -14,6 +14,8 @@ module button_pulse(
     begin
         Q_reg <= {Q_reg[N - 2:0], raw_button};
     end
-    
+
+	// only sends a pulse after 2 clock cycles of raw_button == 1
+	// if Q_reg[2:0] == 3'b111 then button_pulse = 0;
     assign button_pulse = (&Q_reg[N - 2:0]) & ~Q_reg[N-1];
 endmodule
